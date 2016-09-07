865e2b8a0dc8f0b2fb907229a93a8423c365f0b9
DavidNester <davidnester@Davids-MacBook-Pro.local>
2016-09-06 21:19:21 -0400

    added trackbar, video changes with it

--- GUIvideo.py
+++ GUIvideo.py
@@ -1,14 +1,27 @@
 import numpy as np
 import cv2
 
+"""
+Function called when track bar is moved
+Changes video frame along with movement
+"""
+def onChanged(x):
+    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,x)
+    err,img = cap.read()
+    cv2.imshow('frame', img)
+
 cap = cv2.VideoCapture('pendulum.MOV')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame',gray)
+    
+    cv2.createTrackbar('Frames','frame',0,int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)),onChanged)
+    cv2.setTrackbarPos('Frames','frame',int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)))
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
