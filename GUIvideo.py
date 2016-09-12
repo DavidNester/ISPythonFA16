c3e85641f11c85570853118fe6626cdb02ae015f
DavidNester <davidnester@Davids-MacBook-Pro.local>
2016-09-12 01:48:06 -0400

    added pause functionality

--- GUIvideo.py
+++ GUIvideo.py
@@ -10,20 +10,28 @@ def onChanged(x):
     err,img = cap.read()
     cv2.imshow('frame', img)
 
+
 cap = cv2.VideoCapture('pendulum.MOV')
 
 font = cv2.FONT_HERSHEY_SIMPLEX
+pause = False
 
 while(cap.isOpened()):
-    ret, frame = cap.read()
+    if not pause:
+        ret, frame = cap.read()
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame',gray)
     cv2.createTrackbar('Frames','frame',0,int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)),onChanged)
     cv2.setTrackbarPos('Frames','frame',int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)))
-    cv2.putText(gray,str(int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))),(100,100), font, 5,(0,0,0))
-    
+    cv2.putText(gray,str(int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))),(int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))/2,int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))/2), font, 5,(255,0,255))
+    print str(int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)))
+    if cv2.waitKey(1) & 0xFF == ord('p'):
+        if pause:
+            pause = False
+        else:
+            pause = True
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
