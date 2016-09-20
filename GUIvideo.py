4dce798acd6456b833e70e2386f3386316db022c
Kyle <kyle.johnson@emu.edu>
2016-09-20 12:00:29 -0400

    Revert "Updates to make code run smoother"
    This reverts commit fa2fb0406dbf513d969fb0a3a033a98693473de4.

--- GUIvideo.py
+++ GUIvideo.py
@@ -8,6 +8,9 @@ Function called when track bar is moved
 Changes video frame along with movement
 """
 def onChanged(x):
+    global finalFrame
+    #allow video to start again after movement if we have reached the final frame
+    finalFrame = False
     #set video to frame corresponding to slider bar
     cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,x)
     err,img = cap.read()
@@ -16,13 +19,12 @@ def onChanged(x):
     #add frame number
     cv2.putText(gray,str(x),(0,int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))), font, 2,(255,255,255))
 
-global option, color
 
 rect = (0,0,0,0)
 startPoint = False
 endPoint = False
 option = 1
-color = (255, 0, 0)
+color = (255, 0, 255)
 
 """Function called when the image is clicked on"""
 def on_mouse(event,x,y,flags,params):
@@ -48,9 +50,9 @@ def lineChoice():
     global option
     option = 2
 
-def blackColor():
+def redColor():
     global color
-    color = (0,0,0)
+    color = (255,0,0)
 
 def whiteColor():
     global color
@@ -63,7 +65,7 @@ class MyWindow(QtGui.QDialog):    # any super class is okay
         layout = QtGui.QGridLayout()
         circleButton = QtGui.QPushButton('Circle')
         lineButton = QtGui.QPushButton('Line')
-        blackButton = QtGui.QPushButton('Black')
+        redButton = QtGui.QPushButton('Red')
         whiteButton = QtGui.QPushButton('White')
         label1 = QtGui.QLabel("Shape")
         label2 = QtGui.QLabel("Color")
@@ -71,11 +73,11 @@ class MyWindow(QtGui.QDialog):    # any super class is okay
         layout.addWidget(circleButton ,1,0)
         layout.addWidget(lineButton ,1,1)
         layout.addWidget(label2, 2, 0)
-        layout.addWidget(blackButton,3,0)
+        layout.addWidget(redButton,3,0)
         layout.addWidget(whiteButton,3,1)
         circleButton.clicked.connect(circleChoice)
         lineButton.clicked.connect(lineChoice)
-        blackButton.clicked.connect(blackColor)
+        redButton.clicked.connect(redColor)
         whiteButton.clicked.connect(whiteColor)
         self.setLayout(layout)
         self.setWindowTitle("Toolbar");
@@ -83,66 +85,100 @@ class MyWindow(QtGui.QDialog):    # any super class is okay
         # here put the code that creates the new window and shows it.
         child = MyWindow(self)
         child.show()
-        
+while True:
+    try:
+        fps = int(raw_input("How many frames per second does the video have? "))
+        break
+    except:
+        print "Please enter an Integer value"
 font = cv2.FONT_HERSHEY_SIMPLEX
 pause = False
 cap = cv2.VideoCapture('pendulum.MOV')
 frameMemory = []
-#offset so that dont have to subtract 1 every time we access a frame
+#offset so that we dont have to subtract 1 every time we access a frame
 frameMemory += [-1]
 
 height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-count = 0
+
+finalFrame = False
+
+#variables used to control speed of playback
+speed = 1
+#counts up to speed variable. next frame is returned when counter = speed
+counter = 0
+#wrapper function to get next frame that throttles based on speed variable and counter
+def getFrame():
+    global counter, frame
+    #stop getting new frames if we are on the final frame
+    if not finalFrame:
+        #if not paused get frame
+        if not pause:
+            if (counter + 1) == speed:
+                ret, frame = cap.read()
+                counter = 0
+            else:
+                counter += 1
 
 while(cap.isOpened()):
+    if cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT) == cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES):
+        finalFrame = True
+    #global option, color
     cv2.namedWindow('frame')
-    #get next frame if not paused
-    if not pause:
-        ret, frame = cap.read()
+    getFrame()
     #switch to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #add frame number to video
     cv2.putText(gray,str(int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))),(0,height), font, 2,(255,255,255))
+    #add seconds to video
+    cv2.putText(gray,str("{0:.2f}".format(float(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)/float(fps))))+'s',(0,height-50), font, 1,(255,255,255))
     #create trackbar with length = to the number of frames, linked to onChanged function
     cv2.createTrackbar('Frames','frame',0,int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)),onChanged)
     #sets the trackbar position equal to the frame number
     cv2.setTrackbarPos('Frames','frame',int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)))
     
     cv2.setMouseCallback('frame', on_mouse) 
-    
+    #pause
     if cv2.waitKey(1) & 0xFF == ord('p'):
         if pause:
             pause = False
         else:
             pause = True
+    #quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    #slower
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        speed *= 2
+    #faster
+    if cv2.waitKey(1) & 0xFF == ord('e'):
+        if speed != 1:
+            speed /= 2
+    #drawing options
     if cv2.waitKey(1) & 0xFF == ord('t'):
-        if count == 0:
-            """Code for creating windows"""
-            # QApplication created only here.
-            app = QtGui.QApplication([])
-            window = MyWindow()
-            window.show()
-            count += 1
-            """End"""
-        elif count > 0:
-            window.show()
-            
+        """Code for creating windows"""
+        # QApplication created only here.
+        app = QtGui.QApplication([])
+        window = MyWindow()
+        window.show()
+        app.exec_()
+        """End"""
 
     """Code for drawing on video"""
 
     #drawing line
-    if option == 2:
-        cv2.line(gray, (rect[0], rect[1]), (rect[2], rect[3]),color, 2)
-    elif option == 1:
-        cv2.circle(gray, (rect[0], rect[1]), 50,color, -1)
+    if startPoint == True and endPoint == True:
+        if option == 2:
+            cv2.line(gray, (rect[0], rect[1]), (rect[2], rect[3]), color, 2)
+        elif option == 1:
+            cv2.circle(gray, (rect[0], rect[1]), 50, color, -1)
         
     for i in range(1+(height/100)):
         cv2.line(gray,(0,i*100),(width,i*100),(0,255,255),1)
     for i in range(1+(width/100)):
         cv2.line(gray,(i*100,0),(i*100,height),(0,255,255),1)
+        
+    gray = cv2.resize(gray,(0,0),fx=2,fy=2)
     cv2.imshow('frame', gray)
     #put frame in memory array indexed by frame number
     frameMemory += [gray]
