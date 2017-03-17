2befad961ced9272f0589bfd917ba5db9efe3d05
Kyle <kyle.johnson@emu.edu>
2017-03-17 14:21:27 -0400

    lastest update
    3/17

--- davidtest.py
+++ davidtest.py
@@ -31,7 +31,7 @@ reset = ""
 height = 0
 width = 0
 radio = 0
-currentFrame = 1
+count = 1
 size = 120
 speed = 0
 plot = False
@@ -60,6 +60,7 @@ xLine = 0
 yLine = 0
 input = ""
 tempdir = ""
+title = ""
 xPlot = ""
 w = ""
 yPlot = ""
@@ -84,26 +85,20 @@ def quit_(root):
     root.destroy()
 
   
- 
 def update_image(image_label, list, count):
    global tracker, pause, plot, size, xAxis, yAxis,canvas, size_pixel, frame, f,xLine,yLine,height,width, xCoords, var
    frame = list[count]
- 
-def update_image(image_label, video, currentFrame):
-   global xAxis,yAxis,canvas,f,xLine,yLine,height,width,frame
-   frame = video[currentFrame]
- 
    if height == 0:
        height = len(frame)
        width = len(frame[0])
    
    #if we already have the frame in memory then use circles that were found
-   if currentFrame in tracker.coords.keys():
-       x,y,r = tracker.coords[currentFrame]
+   if count in tracker.coords.keys():
+       x,y,r = tracker.coords[count]
    
    #find new circles if new frame and not paused
    elif not pause:
-       frame,lost = tracker.find(frame,currentFrame,pause)
+       frame,lost = tracker.find(frame,count,pause)
        if lost:
            bottom.config(text='Circle is lost. Please click on the center')
            pauseVideo()
@@ -122,64 +117,45 @@ def update_image(image_label, video, currentFrame):
           yLine, = yAxis.plot(tracker.getTCoords(),tracker.getYCoords(),'ro')
           yAxis.draw_artist(yAxis.patch)
           yAxis.draw_artist(yLine)
- 
- 
-      if currentFrame == len(video)-1:
-          xCoords = tracker.getXCoords()
-          print "yp"
- 
       f.canvas.draw()
    
-   im = cv2.cvtColor(video[currentFrame], cv2.COLOR_BGR2RGB)
+   im = cv2.cvtColor(list[count], cv2.COLOR_BGR2RGB)
    a = Image.fromarray(im)
    b = ImageTk.PhotoImage(image=a)
    image_label.configure(image=b)
    image_label._image_cache = b  #avoid garbage collection
    root.update()
 
- 
 def update_all(root, image_label, list):
    global count, speed, bottom, xCoords, tracker, w
- 
-def update_all(root, image_label, video):
-   global currentFrame
- 
    if speed < 0:
        time.sleep((speed*.1)*-1) 
    elif speed > 0:
-       currentFrame = currentFrame + (1*speed)
-   if pause == False and currentFrame < len(video):
-       currentFrame += 1
-       w.set(currentFrame)
-       update_image(image_label, video, currentFrame)
-       root.after(0, func=lambda: update_all(root, image_label, video))
+       count = count + (1*speed)
+   if pause == False and count < len(list):
+       count += 1
+       w.set(count)
+       update_image(image_label, list, count)
+       root.after(0, func=lambda: update_all(root, image_label, list))
        
 
 #multiprocessing image processing functions-------------------------------------
- 
 def image_capture(list):
    global tempdir
- 
-def image_capture(video):
- 
    vidFile = cv2.VideoCapture(tempdir)
    while True:
       try:
          flag, frame=vidFile.read()
          if flag==0:
              break
-         video.append(frame)
+         list.append(frame)
       except:
          continue
 
 """Function called when the image is clicked on"""
 """used to get user input on location when no object is found by clicking center and outside of object"""
 def on_mouse(event):
- 
     global center,outside,currentFrame,tracker,pause,first,points,ax,plot,frame,list,count, image_label
- 
-    global center,outside,tracker,pause,first,frame
- 
     #get only left mouse click
     x=event.x
     y=event.y
@@ -190,16 +166,17 @@ def on_mouse(event):
         if center is not None:
             outside = (x,y)
             if first is None:
-                first = (center[0],center[1],distance(center,outside),currentFrame)
+                first = (center[0],center[1],distance(center,outside),count)
+            
             
             x,y = center
             r = distance(center,outside)
-            tracker.insert(x,y,r,currentFrame)
+            tracker.insert(x,y,r,count)
             cv2.circle(frame, (x, y), r, (228, 20, 20), 4)
             cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            tracker.lastFrameWith = currentFrame
+            tracker.lastFrameWith = count
             #return to normal state
-            playVideo(root,image_label,video)
+            playVideo(root,image_label,list)
             center = outside = None
             bottom.config(text='')
         #if first click (center)
@@ -208,19 +185,19 @@ def on_mouse(event):
             cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
             bottom.config(text='Click on the outside of the circle')
                  
-def playVideo(root, image_label, video):
+def playVideo(root, image_label, list):
     global pause
     pause = False
-    update_all(root, image_label, video)
+    update_all(root, image_label, list)
     
 def pauseVideo():
     global pause
     pause = True
     
-def updateCurrentFrame(image_label, video):
-    global currentFrame
-    currentFrame = w.get()
-    update_image(image_label, video, currentFrame)
+def updateCount(image_label, list):
+    global count
+    count = w.get()
+    update_image(image_label, list, count)
     
 def slowDown():
     global speed
@@ -231,13 +208,9 @@ def fastForward():
     speed += 1
 
 def reset():
- 
     global radio,count,size,speed, var,plot,xCoords,rCoords,yCoords,tCoords,size_pixel,r_pixel,var,f,xAxis,yAxis,fps,xdistance_cm,ydistance_cm,xdistance_in,ydistance_in,center,outside,first,bottom,frame,pause, xLine,yLine,tracker
- 
-    global radio,currentFrame,size,speed,plot,xCoords,rCoords,yCoords,tCoords,size_pixel,r_pixel,var,f,xAxis,yAxis,fps,xdistance_cm,ydistance_cm,xdistance_in,ydistance_in,center,outside,first,bottom,frame,pause, xLine,yLine,tracker
- 
     radio = 0
-    currentFrame = 1
+    count = 1
     size = 120
     speed = 0
     plot = False
@@ -266,7 +239,7 @@ def reset():
     yLine = 0
 
     tracker = CircleTracker()
-    update_all(root,image_label,video)
+    update_all(root,image_label,list)
 
 def submitData():
     global size, bottom, input, information, submit
@@ -288,19 +261,19 @@ def displayChoice():
     
     if var.get()==1:
         xAxis = f.add_subplot(111)
-        xAxis.set_xlim([0,len(video)])
+        xAxis.set_xlim([0,len(list)])
         xAxis.set_ylim([0,width])
     elif var.get() == 2:
         yAxis = f.add_subplot(111)
-        yAxis.set_xlim([0,len(video)])
+        yAxis.set_xlim([0,len(list)])
         yAxis.set_ylim([0,height])
     elif var.get() == 3:
         f = Figure(figsize=(10,5), dpi=100)
         xAxis = f.add_subplot(121)
-        xAxis.set_xlim([0,len(video)])
+        xAxis.set_xlim([0,len(list)])
         xAxis.set_ylim([0,width])
         yAxis = f.add_subplot(122)
-        yAxis.set_xlim([0,len(video)])
+        yAxis.set_xlim([0,len(list)])
         yAxis.set_ylim([0,height])
    
     canvas = FigureCanvasTkAgg(f, master=root)
@@ -309,57 +282,46 @@ def displayChoice():
  
   
 def exportData():
-    xCoords = tracker.getXCoords()
-    yCoords = tracker.getYCoords()
-    tCoords = tracker.getTCoords()
+    global xCoords, yCoords, tracker
+    xCoords = tracker.getXCoords
+    yCoords = tracker.getYCoords
     workbook = xlwt.Workbook()
     worksheet = workbook.add_sheet('Data')
-    worksheet.write(0, 0, 'Frame')
-    worksheet.write(0, 1, 'X Axis')
-    worksheet.write(0, 2, 'Y Axis')
-    
-    num = 1
-    for t in tCoords:
-        worksheet.write(num, 0, t)
-        num += 1
-
-    num = 1
-    for x in xCoords:
-        worksheet.write(num, 1, x)
-        num += 1
+    worksheet.write(0, 0, 'X Axis')
+    worksheet.write(0, 1, 'Y Axis')
        
-    num = 1
+    xCoords = tracker.getXCoords()
+    count = 1
+    for x in xCoords:
+        worksheet.write(count, 0, x)
+        count += 1
+       
+    count = 1
+    yCoords = tracker.getYCoords()
     for y in yCoords:
-       worksheet.write(num, 2, y)
-       num += 1
-
+       worksheet.write(count, 1, y)
+       count += 1
+   
+    file_name = tkFileDialog.asksaveasfile(mode='w', defaultextension=".xls")
+    print file_name
     workbook.save('array.xls')  #this wasnt working
-    print "workbook"
- 
 
 def open():
-   global tempdir, xPlot, yPlot, bothPlot, displayPlot, var, input, reset, information, submit, image_label
+   global tempdir, title, xPlot, yPlot, bothPlot, displayPlot, var, input, reset, information, submit, image_label, w
    openButton.destroy()
+   title.destroy()
+   
    """INPUT FILE"""
    currdir = os.getcwd() #sets current directory
    tempdir = tkFileDialog.askopenfilename( filetypes = (("Movie files", "*.MOV"), ("HTML files", "*.html;*.htm"),("All files", "*.*"))) #requests file name and type of files
 
- 
-    
-  
-if __name__ == '__main__':
-   video = list()
-   root = Toplevel()
-   root.wm_title("Object Tracker")
-   
- 
    var = IntVar()
    image_label = Label(master=root) #label for the video frame
    image_label.grid(row=0, column=0, columnspan=4)
-   p = image_capture(video) #this line takes a rally long time
+   p = image_capture(list)
    image_label.bind('<Button-1>',on_mouse)
-   holder = Frame(master=root)
    
+   holder = Frame(master=root)
    xPlot = Radiobutton(master=holder, text="X Axis", variable=var, value=1)
    xPlot.pack(side="top")
    
@@ -373,11 +335,11 @@ if __name__ == '__main__':
    displayPlot.pack(side="top")
    holder.grid(row=0, column=4, rowspan=4)
    
-   size = len(video)
-   update_image(image_label, video, 0)
+   size = len(list)
+   update_image(image_label, list, 0)
    slider_width = image_label.winfo_width()
    w = Scale(master=root, from_=0, to=size, orient=HORIZONTAL, length=slider_width)
-   w.bind("<ButtonRelease-1>", lambda event: updateCurrentFrame(image_label, video))
+   w.bind("<ButtonRelease-1>", lambda event: updateCount(image_label, list))
    w.grid(row=1, column=0, columnspan=4)
 
    # pause button
@@ -385,7 +347,7 @@ if __name__ == '__main__':
    pauseButton.grid(row=2, column=0)
    
    #play button
-   playButton = Button(master=root, text="Play", command= lambda: playVideo(root, image_label, video))
+   playButton = Button(master=root, text="Play", command= lambda: playVideo(root, image_label, list))
    playButton.grid(row=2, column=1)
    
    #slow down
@@ -405,17 +367,16 @@ if __name__ == '__main__':
    submit = Button(master=root, text='Submit', command=submitData)
    submit.grid(row=3, column=3)
    
-   export = Button(master=root, text='Export', bg='red', command= lambda: exportData())
+   export = Button(master=root, text='Export', command= lambda: exportData())
    export.grid(row = 4, column = 1)
    
-   close = Button(master=root, text='Close',bg='red', command= lambda: quit_(root))
-   close.grid(row = 4, column = 3)
+   end = Button(master=root, text='End', command= lambda: quit_(root))
+   end.grid(row = 4, column = 3)
    
-   reset = Button(master=root,bg='red', text='Reset', command = reset)
+   reset = Button(master=root, text='Reset', command = reset)
    reset.grid(row = 4, column = 2)
    
    # setup the update callback
- 
    root.after(0, func=lambda: update_all(root, image_label, list))    
   
 if __name__ == '__main__':
@@ -425,11 +386,12 @@ if __name__ == '__main__':
    root = Toplevel()
    root.wm_title("Object Tracker")
 
+   title = Label(master=root, text="Welcome to the Object Tracker!")
+   title.config(font=("Courier", 30))
+   title.grid(row=0, column=0, padx=100,pady=50)
+   
    openButton = Button(master=root, text="OPEN VIDEO", command= lambda: open())
-   openButton.grid(row=0, column=0, padx=100, pady=100)
- 
-   root.after(0, func=lambda: update_all(root, image_label, video))
- 
+   openButton.grid(row=1, column=0, padx=100, pady=50)
    
    root.lift()
    root.attributes('-topmost',True)
@@ -440,7 +402,6 @@ if __name__ == '__main__':
 """master = Tk()
 master = Toplevel()
 master.wm_title("Object Tracker")
-
 resultx = StringVar()
 resulty = StringVar()
 xdistance_cm = tracker.xMax() - tracker.xMin()
@@ -452,6 +413,4 @@ resultx.set(responsex)
 resulty.set(responsey)
 Label(master, textvariable=resultx).grid(row=0)
 Label(master, textvariable=resulty).grid(row=1)
-
-
 mainloop()"""
