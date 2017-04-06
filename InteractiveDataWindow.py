b53d4628a19fbd9c40326795e5db03c31896531a
DavidNester <davidnester@Davids-MacBook-Pro.local>
2017-04-06 19:01:45 -0400

    more interpolation options

--- InteractiveDataWindow.py
+++ InteractiveDataWindow.py
@@ -1,7 +1,8 @@
 from Tkinter import *
 import tkFileDialog
 import matplotlib
-
+import numpy as np
+from scipy.interpolate import CubicSpline
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -40,6 +41,17 @@ class InteractiveDataWindow:
         self.yAccButton = Button(master=self.data,text='y Acceleration', command = self.yAccData)
         self.yAccButton.grid(row=6,column=0,columnspan=2)
     
+        self.fitXButton = Button(master=self.data,text='Fit X', command = self.fitX)
+        self.fitXButton.grid(row=0,column=2,columnspan=2)
+    
+        self.fitYButton = Button(master=self.data,text='Fit Y', command = self.fitY)
+        self.fitYButton.grid(row=1,column=2,columnspan=2)
+    
+        self.fitXButton = Button(master=self.data,text='Fit X Velocity', command = self.fitXVelocity)
+        self.fitXButton.grid(row=2,column=2,columnspan=2)
+        
+        self.fitYButton = Button(master=self.data,text='Fit Y Velocity', command = self.fitYVelocity)
+        self.fitYButton.grid(row=3,column=2,columnspan=2)
 
     def plotData(self,x,y,num):
         plt.figure(num)
@@ -64,3 +76,23 @@ class InteractiveDataWindow:
     def yAccData(self):
         x,y = self.tracker.yAcceleration()
         self.plotData(x,y,7)
+    def fitX(self):
+        self.trendline(self.tracker.getTCoords(),self.tracker.getXCoords(),8)
+    def fitY(self):
+        self.trendline(self.tracker.getTCoords(),self.tracker.getYCoords(),9)
+    def fitXVelocity(self):
+        x,y = self.tracker.xVelocity()
+        self.trendline(x,y,10)
+    def fitYVelocity(self):
+        x,y = self.tracker.yVelocity()
+        self.trendline(x,y,11)
+
+    def trendline(self,x,y,num):
+        plt.figure(num)
+        cs = CubicSpline(x,y)
+        plt.plot(x, cs(x), label="S")
+        #plt.plot(x, cs(x, 1), label="S'")
+        #plt.plot(x, cs(x, 2), label="S''")
+        #plt.plot(x, cs(x, 3), label="S'''")
+        plt.show()
+    
